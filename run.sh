3562817df5424a9575993a5ffaf547ee2d95dea8
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-07-11 15:05:39 -0400

    Adding script to generate claat docs

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	if [ ${#CLAAT_AUTH_CODE} = 0 ]
+	then
+		echo "Using system CLAAT_AUTH_CODE"
+		./claat export "$line"
+	else
+		echo "Using user defined CLAAT_AUTH_CODE"
+		./claat export -auth $CLAAT_AUTH_CODE "$line"
+	fi
+    echo "Exported: $line"
+done < "$1"
+rm -rf logs
+mkdir logs
+./claat 
+nohup ./claat serve > logs/claat.log&
+echo "Serving on port 9090"
