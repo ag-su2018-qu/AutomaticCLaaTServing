84d793a4cfd827c7a011ca32c5089cf0a4daf5b9
ag-su2018-qu <ag.su2018.qu@gmail.com>
2018-07-13 11:59:58 -0400

    Updating script and adding script to download claat

---
+++ pre_execution/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo rm -rf claat-bin
+mkdir claat-bin
+cd claat-bin
+wget https://github.com/googlecodelabs/tools/releases/download/v1.0.4/claat-linux-amd64
+mv claat-linux-amd64 claat
+sudo chmod +x claat
+dir=$(pwd)
+export PATH=$dir
+source ~/.bashrc
--- run.sh
+++ run.sh
@@ -3,15 +3,15 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
 	if [ ${#CLAAT_AUTH_CODE} = 0 ]
 	then
 		echo "Using system CLAAT_AUTH_CODE"
-		./claat export "$line"
+		claat export "$line"
 	else
 		echo "Using user defined CLAAT_AUTH_CODE"
-		./claat export -auth $CLAAT_AUTH_CODE "$line"
+		claat export -auth $CLAAT_AUTH_CODE "$line"
 	fi
     echo "Exported: $line"
 done < "$1"
 rm -rf logs
 mkdir logs
-./claat 
-nohup ./claat serve > logs/claat.log&
+claat 
+nohup claat serve > logs/claat.log&
 echo "Serving on port 9090"
