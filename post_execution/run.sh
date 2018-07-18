c21ffb7142861f9f6c274ce877a119e85aefb6da
ag-su2018-qu <39270554+ag-su2018-qu@users.noreply.github.com>
2018-07-18 11:40:48 -0400

    Updating post execution script

--- post_execution/run.sh
+++ post_execution/run.sh
@@ -3,8 +3,8 @@ source ~/.profile
 dir=$(pwd)
 if [ -d "${dir}/data" ] 
 then
-	echo "Running sh ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv"
-	sudo bash "${dir}/AutomaticCLaaTServing/run.sh" "${dir}/data/test-data.csv"
+	echo "Running bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv"
+	sudo bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv
 else
 	echo "data directory does not exist"
 fi
