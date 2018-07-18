3ae05f6d1c076ac8062b6c0fbfc41d6a0c890801
ag-su2018-qu <39270554+ag-su2018-qu@users.noreply.github.com>
2018-07-18 16:07:17 -0400

    Update

--- post_execution/run.sh
+++ post_execution/run.sh
@@ -5,6 +5,7 @@ if [ -d "${dir}/data" ]
 then
 	echo "Running bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv"
 	sudo bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv
+	sudo nohup python3 -m http.server 80 >> logs/claat.log &
 else
 	echo "data directory does not exist"
 fi
