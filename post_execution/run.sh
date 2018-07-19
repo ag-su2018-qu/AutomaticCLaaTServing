f980b7b03d99ebaf2253a933bbc46ba4b4a0ba0e
hs-su2018-qu <39773785+hs-su2018-qu@users.noreply.github.com>
2018-07-19 11:11:29 -0400

    remove sudo access

--- post_execution/run.sh
+++ post_execution/run.sh
@@ -4,7 +4,7 @@ dir=$(pwd)
 if [ -d "${dir}/data" ] 
 then
 	echo "Running bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv"
-	sudo bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv
+	bash ${dir}/AutomaticCLaaTServing/run.sh ${dir}/data/test-data.csv
 	sudo nohup python3 -m http.server 80 >> logs/claat.log &
 else
 	echo "data directory does not exist"
